e5adc55205b7b8dba7d5293e18f02a463f4d1976
hsmak <husain.alkhamis@gmail.com>
2020-12-21 14:57:13 -0600

    committing changes..

--- __etc/update-forks.sh
+++ __etc/update-forks.sh
@@ -1,1 +1,1 @@
-$ git subtree pull --prefix reactive-essentials/reactor-project-solution https://github.com/reactor/lite-rx-api-hands-on.git master
+git subtree pull --prefix reactive-essentials/reactor-project-mysolution https://github.com/reactor/lite-rx-api-hands-on.git master
